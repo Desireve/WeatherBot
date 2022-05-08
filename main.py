6887cc518b068bcd437be11b83e88e03d64b273f
Desireve <55155824+Desireve@users.noreply.github.com>
2022-05-08 23:24:35 +0500

    Update main.py
    test 2

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
 
+
+
+
 # команда
 @dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+async def start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши название города, а я пришлю и")
+
+@dp.message_handler()
+async def pogoda_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+    name_gorod = msg.text
 
 
 # запускаем лонг поллинг
