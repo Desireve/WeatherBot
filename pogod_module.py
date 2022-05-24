4af091e2de1e3437b3a639ebb337e5d29be9ddd9
Desireve <55155824+Desireve@users.noreply.github.com>
2022-05-25 01:16:34 +0500

    Update pogod_module.py
    переписал модуль погоды

--- pogod_module.py
+++ pogod_module.py
@@ -1,18 +1,34 @@
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
-from torch_optimizer import get
 from config import POGODA_TOKEN
  
 owm = OWM(POGODA_TOKEN)
 mgr = owm.weather_manager()
 
 
-def get_pogoda(gorod_name):
+def get_temp(gorod_name):
     observa = mgr.weather_at_place(gorod_name + ',RU')
     weather1 = observa.weather
     temp = weather1.temperature('celsius')
 
     return temp
+
+def get_osadki_status(gorod_name):
+    observa = mgr.weather_at_place(gorod_name + ',RU')
+    weather1 = observa.weather
+    rain = weather1.detailed_status
+
+    return rain
+
+def get_wind_speed(gorod_name):
+    observa = mgr.weather_at_place(gorod_name + ',RU')
+    weather1 = observa.weather.wind()
+
+    windspeed  = weather1.get('speed')
+
+    return windspeed
+
+
 
 
